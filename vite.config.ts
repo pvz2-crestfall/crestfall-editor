e344de4bc16ee3c0685c2c7a958d73dcf48e7b4b
ZackGabri <44709674+ZackGabri@users.noreply.github.com>
2025-11-01 22:15:15 +0200

    Change base in case it's running on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
+// Set the base to be the repository name in the case it's running on GitHub Actions
+// otherwise just default to '/'
+const base = process.env.GITHUB_ACTIONS === 'true' ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1]}/` : '/';
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), ViteImageOptimizer()],
@@ -12,4 +16,6 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
+
+    base,
 });

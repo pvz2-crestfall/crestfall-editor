ea74d8cfc39be5a3b80e1e4ffeed3217952c15bb
ZackGabri <44709674+ZackGabri@users.noreply.github.com>
2026-03-27 10:29:19 +0200

    Add more graveestone types

--- src/components/grave-list.ts
+++ src/components/grave-list.ts
@@ -1,7 +1,12 @@
 export const gravestoneList: Record<string, { label: string; icon?: string }> = {
     default: { label: 'Default Tombstone', icon: 'gravestone_unknown' },
     gravestone_tutorial: { label: 'Basic Tombstone' },
+    gravestone_cowboy: { label: 'Cowboy Tombstone' },
     gravestone_egypt: { label: 'Egypt Tombstone' },
+    gravestone_future: { label: 'Future Tombstone' },
+    gravestone_iceage: { label: 'Frostbite Caves Tombstone' },
+    gravestone_lostcity: { label: 'Lost City Tombstone' },
+    gravestone_pirate: { label: 'Pirate Tombstone' },
     gravestone_dark: { label: 'Dark Ages Tombstone' },
     gravestonePlantfoodOnDestruction: { label: 'Plant Food Tombstone' },
     gravestoneSunOnDestruction: { label: 'Sun Tombstone' },
